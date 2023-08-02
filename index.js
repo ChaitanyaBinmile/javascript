2454de565e8388aa0dc851f2f06f5fb796d9f287
Chaitanya <chaitanya.v@binmile.com>
2023-08-02 16:34:46 +0530

    sa

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict"
+// s=1 
+console.log('sas')
+
+// VARIABLES
+var x = 5;
+var y = 6;
+var z = 7;
+
+// Numbers:
+let height = 16;
+let weight = 7.5;
+
+// Strings:
+let firstName = "rock";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {class:"11", subject:"science"};
+
+// Array object:
+const cars = ["maruti", "hundai", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-28");
+
+// Methods of primitives
+let str = "Welcome to W3Docs";
+console.log(str.toUpperCase()); 
+
+
+// Array Methods
+// push pop shift unshift
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
