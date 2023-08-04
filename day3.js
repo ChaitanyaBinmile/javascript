aba6f9b9ed28ad0b3594da23b57bb14f138e4b41
Chaitanya <chaitanya.v@binmile.com>
2023-08-04 13:09:02 +0530

    dfd

---
+++ day3.js
@@ -0,0 +1,29 @@
+// functions
+
+var s = 5;
+function valuechange(s){
+    s=4
+   
+   
+}
+s=valuechange(s)
+console.log(s)
+
+
+function print(s){
+    console.log(s)
+}
+
+print("dss")
+
+
+// funciton expressions
+
+const expression = (c) =>  c+2;
+
+print(expression(9))
+hola(9)
+
+function hola  (a) {return a+1};
+
+// note function expressions cannot be hoisted but normal functions can
